02c8a6d7527a1df103335fee1af902e6211009aa
yeunarnd <ynarnd23@gmail.com>
2021-12-06 21:27:49 +0700

    gambar

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -27,16 +27,17 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-lg-7">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-2 d-none d-lg-block">
-                                <img src="<?= base_url('back-end/img/gb-login.jpg') ?>" width="250" alt="">
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <img src="<?= base_url('back-end/img/gb-login.jpg') ?>" width="500" alt="">
                             </div>
-                                <div class="p-5">
+                            <div class="col-lg-6">
+                            <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
@@ -56,7 +57,7 @@
                                     </form>
 
                                     <hr>
-                                    <h1 style="text-align: center;">Pos PAUD Sri Rejeki</h1>
+                                    <h2 style="text-align: center;">Pos PAUD Sri Rejeki</h2>
                                     <!-- <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
@@ -64,7 +65,7 @@
                                         <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
                                     </div> -->
                                 </div>
-                            
+                            </div>
                         </div>
                     </div>
                 </div>
