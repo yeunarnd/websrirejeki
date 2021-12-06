66d53b34eafec69b32d3be7e7bbc716acea00f73
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2021-12-06 21:25:11 +0700

    1

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -33,14 +33,14 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-2 d-none d-lg-block">
-                                <img src="<?= base_url('back-end/img/gb-login.jpg') ?>" width="250" alt="">
-                            </div>
+                                        
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-
+                                    <div class="col-lg-2 d-none d-lg-block">
+                                        <img src="<?= base_url('back-end/img/gb-login.jpg') ?>" width="250px" alt="">
+                                        </div>
                                     <?= $this->session->flashdata('msg'); ?>
 
                                     <form class="user" method="POST" action="<?= site_url('login/auth'); ?>">
@@ -54,7 +54,7 @@
                                             Login
                                         </button>
                                     </form>
-
+                                    
                                     <hr>
                                     <h1 style="text-align: center;">Pos PAUD Sri Rejeki</h1>
                                     <!-- <div class="text-center">
