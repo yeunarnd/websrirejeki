18db2e15600d1397f86e507e279e862190967712
yeunarnd <ynarnd23@gmail.com>
2021-12-08 23:40:53 +0700

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -86,9 +86,9 @@
                 <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)"><img src="<?php echo base_url() ?>front-end/assets/img/slide/slide-2.jpg">
                     <div class="carousel-container">
                         <div class="container">
-                            <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-                            <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+                            <!-- <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
+                            <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
+                            <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
                         </div>
                     </div>
                 </div>
@@ -97,9 +97,9 @@
                 <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)"><img src="<?php echo base_url() ?>front-end/assets/img/slide/slide-3.jpg">
                     <div class="carousel-container">
                         <div class="container">
-                            <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
+                            <!-- <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
                             <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+                            <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
                         </div>
                     </div>
                 </div>
