8ea8e90f8a079911c3b8399a8f943a5267b08cfa
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2021-12-12 19:10:40 +0700

    button title

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -156,7 +156,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $this->session->userdata('username'); ?></span>
-                <img src="<?php echo base_url() ?>back-end/img/user.png" width="50" height="25">
+                <img src="<?php echo base_url() ?>back-end/img/user.png" width="25" height="25">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -175,7 +175,7 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+                    Keluar
                 </a>
             </div>
         </li>
