6a33d255d45224e00bc883da521ac02eef579484
yeunarnd <ynarnd23@gmail.com>
2022-08-24 20:20:16 +0700

    Update role-access.php

--- application/views/admin/role-access.php
+++ application/views/admin/role-access.php
@@ -11,6 +11,66 @@
 
             <h5>Role : <?= $role['role']; ?></h5>
 
+            <!-- DataTables -->
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a href="<?php echo site_url('daftar/add') ?>"><i class="fas fa-plus"></i> Tambah Baru</a>
+
+                    <a class="col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+                        <select name="" id="daftar">
+                            <option value="0">Tampil Semua</option>
+                            <option value="1">2022</option>
+                        </select>
+                    </a>
+                    <a href="<?= base_url('daftar/export_pendaftaran') ?>" class="btn btn-secondary" style="float:right"><i class="fa fa-download"></i> Unduh Laporan</a>
+                </div>
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Menu</th>
+                                    <th scope="col">Access</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($daftar as $daftar) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i; ?></th>
+                                        <td>
+                                            <?= $daftar['tgl_daftar']; ?>
+                                        </td>
+                                        <td>
+                                            <?= $daftar['nm_calon_siswa']; ?>
+                                        </td>
+                                        <td>
+                                            <?php if ($daftar['status'] == 1) {
+                                                echo 'Tervalidasi';
+                                            } else if ($daftar['status'] == 2) {
+                                                echo 'Ditolak';
+                                            } else {
+                                                echo 'Menunggu validasi';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td width="250">
+                                            <a href="<?= base_url('daftar/details/') . $daftar['kd_daftar']; ?>" class="badge badge-info"> Detail</a>
+                                            <a href="<?= base_url('daftar/edit/') . $daftar['kd_daftar']; ?>" class="badge badge-success"> Edit</a>
+                                            <a onclick="return confirm('Apakah anda yakin?')" href="<?php echo site_url('daftar/delete/') . $daftar['kd_daftar']; ?>" class="badge badge-danger">Hapus</a>
+                                        </td>
+                                    </tr>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
             <table class="table table-hover">
                 <thead>
                     <tr>
